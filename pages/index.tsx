ff2c305d49bf0509229bc84eb47524c96f850fbe
Elber <elberrd@gmail.com>
2022-04-26 18:33:04 -0400

    First commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,73 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+import { useSession, signOut, getSession } from "next-auth/react";
+//import useRequireAuth from "../lib/useRequireAuth";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const { data: session } = useSession();
+
+  // const session = useRequireAuth();
+  // if (!session) {
+  //   return <div>loading...</div>;
+  // }
+
+  const [isButtonLoading, setIsButtonLoading] = useState(false);
+
+  return (
+    <div>
+      <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+        <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+          <h1 className="font-bold text-center text-2xl mb-5">
+            {`Hi ${session?.user?.name}, welcome back!`}
+          </h1>
+          <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+            <div className="px-5 py-7"></div>
+            <div className="p-5">
+              <div className="grid">
+                <Button
+                  onClick={() => (setIsButtonLoading(true), signOut())}
+                  //className="bg-blue-600 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded"
+                  isLoading={isButtonLoading}
+                >
+                  Sair do Sistema
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+function Button({ isLoading, children, ...props }) {
+  return (
+    <button
+      className="bg-blue-600 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded"
+      {...props}
+    >
+      {isLoading ? "Saindo..." : children}
+    </button>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
 
 export default Home;
---
+++ pages/login.tsx
@@ -0,0 +1,107 @@
+import { getSession, signIn } from "next-auth/react";
+import React from "react";
+//import { useState } from "react";
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: { session },
+  };
+}
+
+// function Button({ isLoading, children, ...props }) {
+//   return (
+//     <button
+//       className="bg-blue-600 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded"
+//       {...props}
+//     >
+//       {isLoading ? "Logando..." : children}
+//     </button>
+//   );
+// }
+
+function login() {
+  //const [isButtonLoading, setIsButtonLoading] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+      <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+        <h1 className="font-bold text-center text-2xl mb-5">
+          Learning NextAuth
+        </h1>
+        <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+          <div className="px-5 py-7"></div>
+          <div className="p-5">
+            <div className="grid">
+              <button
+                type="button"
+                onClick={() => signIn("github")}
+                className="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center inline-block"
+              >
+                Github
+              </button>
+            </div>
+          </div>
+          <div className="py-5">
+            <div className="grid grid-cols-2 gap-1">
+              <div className="text-center sm:text-left whitespace-nowrap">
+                <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="w-4 h-4 inline-block align-text-top"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"
+                    />
+                  </svg>
+                  <span className="inline-block ml-1">Forgot Password</span>
+                </button>
+              </div>
+              <div className="text-center sm:text-right whitespace-nowrap">
+                <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="w-4 h-4 inline-block align-text-bottom	"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
+                    />
+                  </svg>
+                  <span className="inline-block ml-1">Help</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="py-5">
+          <div className="grid grid-cols-2 gap-1">
+            <div className="text-center sm:text-left whitespace-nowrap"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default login;
